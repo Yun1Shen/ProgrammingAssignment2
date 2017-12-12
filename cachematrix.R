4284f3f2e87f151a72e1c0e79bc5c12d8c58d718
Yun Shen <shenyun0618@gmail.com>
2017-12-12 18:51:00 +0800

    Yun Shen's programmingassignment2 about cache inverse of matrix

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,39 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+## The first function is a list containing a function to
+## set and get the value of the matrix, to set and get the 
+## inverse of the matrix
 makeCacheMatrix <- function(x = matrix()) {
+  invm<-NULL
+  set<-function(y){
+   x<<-y
+   invm<<-NULL
+  }
+  get<-function() x
+  setinv <-function(invx) invm<<-invx
+  getinv <-function() invm
+  list(set=set,get=get,
+       setinv=setinv,
+       getinv = getinv
+  )
 
 }
 
 
 ## Write a short comment describing this function
+## The following function calculate
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  invm<- x$getinv()
+  if(!is.null(invm)){
+    message("getting cached data")
+    return(invm)
+  }
+  data<-x$get()
+  invm<-solve(data,...)
+  x$setinv(invm)
+  invm
+       
 }
